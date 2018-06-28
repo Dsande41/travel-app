447c7d8cdfc494bd76db83f0be9cee99bf6b68d2
Delia Sanders <deliamatu@gmail.com>
2018-06-28 14:34:49 -0400

    wrote a function to getData based on latitude and longitude; includes the request to the foursquare API

--- index.js
+++ index.js
@@ -5,32 +5,55 @@
   const fs= require('fs');
   const filename='data.json';
 
+  const express=require('express');
+  const app=express();
+
+  const Travelapp = require('./db')
+
+  const database=require('./db');
+
+function  getData(lat,long){
   request({
     url: 'https://api.foursquare.com/v2/venues/explore',
     method: 'GET',
     qs: {
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
-      ll: '44.7243,-77.0018',
-      v: '20180323',
+      ll: `${lat},${long}`,
+      v: 20180323,
       limit: 50
     }
-  }, function(err, res, body) {
+  }, function(err, res, data) {
     if (err) {                                                              
       console.log('error');
+
     } else {
-    //   console.log(body.response);
-      fs.writeFile(filename,body,(error)=>{
-        if(error){
-        
-            // console.log(error.message);
-            return;
+        console.log(data);
+        data= JSON.parse(data);
+        for(let i=0; i<data.response.groups[0].items.length;i++){
+            let location_name= data.response.groups[0].items[i].venue.name || '';
+            let latitude=data.response.groups[0].items[i].venue.location.lat;
+            let longitude=data.response.groups[0].items[i].venue.location.lng;
+            let location_address=data.response.groups[0].items[i].venue.location.address || '';
+            let city=data.response.groups[0].items[i].venue.location.city || '';
+            let zipCode=data.response.groups[0].items[i].venue.location.postalCode || 0;
+          
+            database.addLocation(location_name, location_address,zipCode, city);
         }
 
+    //   console.log(body.response);
+    //   fs.writeFile(filename,body,(error)=>{
+    //     if(error){
+        
+    //         // console.log(error.message);
+    //         return;
+    //     }
+
     
-      })
+    //   })
       
     }
   });
-
+}
+ getData(40.718301364039064, -73.99192996850243);
   
