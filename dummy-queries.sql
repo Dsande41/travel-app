19ede72047e55c6a2c32b3d582fcbf398661a650
Delia Sanders <deliamatu@gmail.com>
2018-06-28 14:38:58 -0400

    created dummy-queries for testing purposes

---
+++ dummy-queries.sql
@@ -0,0 +1,16 @@
+INSERT INTO Travelapp(email, username)
+VALUES
+    ('delia@travel.edu', 'delia'),
+    ('mike@digital.com', 'mike'),
+    ('lucky@dog.com','lucky'),
+    ('ginger.thief', 'ginger'),
+    ('chilly.car', 'chilly')
+    ; 
+
+
+INSERT INTO Venues(venuename, venuenumber, street, zipcode,city )
+VALUES
+    ('johnny',67,'north street', 40499,'zambir'),
+    ('coffeespot', 35,'cofee avenue NW', 30567, 'london'),
+    ('sushi', 345, 'fish trails Deep', 345676, 'Miami')
+    ;
