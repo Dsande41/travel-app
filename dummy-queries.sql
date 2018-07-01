3dc3f3b22af7cb435eb47b1da3988bdfa5a7f259
Delia Sanders <deliamatu@gmail.com>
2018-07-01 12:56:17 -0400

    deleted venues table and the Travelapp table, updated  the dummy-queries.sql with the new insert statements

--- dummy-queries.sql
+++ dummy-queries.sql
@@ -1,21 +1,15 @@
-INSERT INTO Travelapp(email, username)
-VALUES
-    ('delia@travel.edu', 'delia'),
-    ('mike@digital.com', 'mike'),
-    ('lucky@dog.com','lucky'),
-    ('ginger.thief', 'ginger'),
-    ('chilly.car', 'chilly')
-    ; 
-
-
-INSERT INTO Venues(venuename, venuenumber, street, zipcode,city )
-VALUES
-    ('johnny',67,'north street', 40499,'zambir'),
-    ('coffeespot', 35,'cofee avenue NW', 30567, 'london'),
-    ('sushi', 345, 'fish trails Deep', 345676, 'Miami')
-    ;
-
-    INSERT INTO Locations(location_name, location_address,city,zipcode, latitude,longitude,category_name )
+ INSERT INTO Locations(location_name, location_address,city,zipcode, latitude,longitude,category_name )
 VALUES
     ('Atlanta pub','1223 north street','Atlanta', 40499,77.1234,-43.66543,'restaurant'),
     ('Central park','1223 South street','portland', 40488,77.1234,-23.66543,'park');
+
+INSERT INTO Users(email_address,user_name)
+VALUES
+    ('User@gmail.com', 'alana'),
+    ('user2@yahoo.con', 'alex');
+
+
+INSERT INTO Itinerary(user_id, location_id, order_item)
+VALUES
+	(2,3,1),
+	(2,5,2);
--- sample-queries.sql
+++ sample-queries.sql
@@ -18,20 +18,9 @@ DELETE FROM Locations
 WHERE location_id=8;
 
 
-INSERT INTO Locations(location_name, location_address,city,zipcode, latitude,longitude,category_name )
-VALUES
-    ('Atlanta pub','1223 north street','Atlanta', 40499,77.1234,-43.66543,'restaurant'),
-    ('Central park','1223 South street','portland', 40488,77.1234,-23.66543,'park');
-
-    
 SELECT * FROM locations
 ORDER BY location_name 
 LIMIT 10;
-
-INSERT INTO Users(email_address,user_name)
-VALUES
-    ('User@gmail.com', 'alana'),
-    ('user2@yahoo.con', 'alex');
 
 
 SELECT * FROM Users;
@@ -50,11 +39,6 @@ WHERE user_name='alex';
 UPDATE Users
 SET email_address = 'test@gmail.com'
 WHERE user_id=3;
-
-INSERT INTO Itinerary(user_id, location_id, order_item)
-VALUES
-	(2,3,1),
-	(2,5,2);
 
 -- update Itinerary to the order_item having a value
 -- and do a join
